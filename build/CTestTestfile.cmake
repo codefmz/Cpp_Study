4a566b1a37fbf984bd44e9cefe2a7b86b2f1baaa
fengmowei <fengmowei@creality.com>
2026-01-16 15:47:44 +0800

    删除无用文件

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /fmw/code/Cpp_Study
-# Build directory: /fmw/code/Cpp_Study/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- build/src/class/CMakeFiles/classT.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for classT.
-# This may be replaced when dependencies are built.
